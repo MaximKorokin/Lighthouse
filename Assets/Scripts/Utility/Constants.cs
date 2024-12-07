4ccb52167646e4d9b62de21985c12ecd158f3bdd
Maxim Korokin <maksimkorokin1@gmail.com>
2024-12-08 01:51:24 +0200

    Add content to Lobby level

--- Assets/Scripts/Utility/Constants.cs
+++ Assets/Scripts/Utility/Constants.cs
@@ -6,9 +6,13 @@ public static class Constants
 
     public const string MainAudioSourceTag = "MainAudioSource";
 
+    /// <summary>
+    /// New and old members must be exact names of scenes
+    /// </summary>
     public enum Scenes
     {
         KinsnapHQAttack = 10,
         KinsnapHQVisit = 11,
+        LobbyFirstVisit = 100,
     }
 }
